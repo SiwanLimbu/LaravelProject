9301aad6dd193bcb4fec4b6a5fa8b2c8d98f1867
ANjitGC741 <anzit741@gmail.com>
2022-12-10 00:06:10 +0545

    admin dashboard side navbar added

---
+++ resources/views/frequently-used/sidebar.blade.php
@@ -0,0 +1,81 @@
+			<nav id="sidebar">
+			  <div class="p-4 pt-5">
+			    <a href="#" class="img logo rounded-circle mb-5" style="background-image: url(images/logo.jpg);"></a>
+			    <ul class="list-unstyled components mb-5">
+			      <li class="active">
+			        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+			        <ul class="collapse list-unstyled" id="homeSubmenu">
+			          <li>
+			            <a href="#">Home 1</a>
+			          </li>
+			          <li>
+			            <a href="#">Home 2</a>
+			          </li>
+			          <li>
+			            <a href="#">Home 3</a>
+			          </li>
+			        </ul>
+			      </li>
+			      <li>
+			        <a href="#">About</a>
+			      </li>
+			      <li>
+			        <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+			        <ul class="collapse list-unstyled" id="pageSubmenu">
+			          <li>
+			            <a href="#">Page 1</a>
+			          </li>
+			          <li>
+			            <a href="#">Page 2</a>
+			          </li>
+			          <li>
+			            <a href="#">Page 3</a>
+			          </li>
+			        </ul>
+			      </li>
+			      <li>
+			        <a href="#">Portfolio</a>
+			      </li>
+			      <li>
+			        <a href="#">Contact</a>
+			      </li>
+			    </ul>
+
+			    <div class="footer">
+			    </div>
+
+			  </div>
+			</nav>
+
+			<!-- Page Content  -->
+			<div id="content" class="">
+
+			  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+			    <div class="container-fluid">
+
+			      <button type="button" id="sidebarCollapse" class="btn btn-primary">
+			        <i class="fa fa-bars"></i>
+			        <span class="sr-only">Toggle Menu</span>
+			      </button>
+			      <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			        <i class="fa fa-bars"></i>
+			      </button>
+
+			      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			        <ul class="nav navbar-nav ml-auto">
+			          <li class="nav-item active">
+			            <a class="nav-link" href="#">Home</a>
+			          </li>
+			          <li class="nav-item">
+			            <a class="nav-link" href="#">About</a>
+			          </li>
+			          <li class="nav-item">
+			            <a class="nav-link" href="#">Portfolio</a>
+			          </li>
+			          <li class="nav-item">
+			            <a class="nav-link" href="#">Contact</a>
+			          </li>
+			        </ul>
+			      </div>
+			    </div>
+			  </nav>
